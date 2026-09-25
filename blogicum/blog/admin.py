9c5bc4716b58e66ff348f822f615b165f824c909
VadimZolotarevAndjil <andjilzolotarev@yandex.ru>
2026-09-25 18:36:13 +0300

    .

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,22 +2,23 @@ from django.contrib import admin
 
 from .models import Post, Category, Location
 
-admin.site.register(Category)
 admin.site.register(Location)
 
 
 class PostModelAdmin(admin.ModelAdmin):
     list_display = (
         "title",
-        "text",
+        "get_short_text",  # Указываем имя метода
         "pub_date"
     )
 
-    def text(self, obj):
-        text = obj.description or ""
+    def get_short_text(self, obj):
+        text = obj.text or ""
         if len(text) > 100:
-            return text [:100] + "…"
+            return text[:100] + "…"
         return text
+    
+    get_short_text.short_description = "Текст"
 
     search_fields = (
         "title",
@@ -35,8 +36,32 @@ class PostModelAdmin(admin.ModelAdmin):
 
 admin.site.register(Post, PostModelAdmin) 
 
-# @admin.register()
-# class CategoryModelAdmin(admin.ModelAdmin):
 
-# @admin.register()
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "get_short_description"
+    )
+
+    def get_short_description(self, obj):
+        description = obj.description or ""
+        if len(description) > 100:
+            return description [:100] + "…"
+        return description
+
+    get_short_description.short_description = "Описание"
+
+    search_fields = (
+        "title",
+    )
+    list_filter = (
+        "slug",
+    )
+
+    list_display_links = (
+            "title",
+        )
+admin.site.register(Category, CategoryModelAdmin)
+
+
 # class LocationModelAdmin(admin.ModelAdmin):
