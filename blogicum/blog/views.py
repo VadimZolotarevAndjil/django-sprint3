361a5d7032765a59d3e9ed267c90378342265a83
VadimZolotarevAndjil <andjilzolotarev@yandex.ru>
2026-09-26 12:02:40 +0300

    .

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -5,44 +5,37 @@ from django.utils import timezone
 
 from blog.models import Post, Category
 
-def get_published_posts():
-    posts = Post.objects.all()
-    posts = posts.filter(
-            is_published=True,
-            category__is_published=True,
-            pub_date__lte=timezone.now(),
-        )
-    return posts
+
+
+def get_published_posts(category=None):
+    qs = Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=timezone.now(),
+    ).select_related("author")
+    if category:
+        qs = qs.filter(category=category)
+    return qs
 
 
 def index(request):
-    template = "blog/index.html"
-    filtered_posts = get_published_posts()
-    post_list = (
-        filtered_posts.select_related("author")
-    )
-    context = {"post_list": post_list}
-    return render(request, template, context)
+    post_list = get_published_posts()
+    return render(request, "blog/index.html", {"post_list": post_list})
 
 
 def post_detail(request, pk):
-    template = "blog/detail.html"
     post = get_object_or_404(
         Post.objects.select_related("category", "author"),
         id=pk,
         is_published=True
     )
-    context = {"post": post}
-    return render(request, template, context)
+    return render(request, "blog/detail.html", {"post": post})
 
 
 def category(request, category_slug):
     template = "blog/category.html"
-    category = Category.objects.get(slug=category_slug)
-    if not category.is_published:
-        raise Http404("Категория не найдена")
-    filtered_posts = get_published_posts()
-    post_list = filtered_posts.filter(category=category).select_related("author")
+    category = get_object_or_404(Category, slug=category_slug, is_published=True)
+    post_list = get_published_posts(category=category)
     context = {
         "category": category,
         "post_list": post_list,
