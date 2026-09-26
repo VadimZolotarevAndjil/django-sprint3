d665ee5775af8f88ff319fca4cb4674878c39cb3
VadimZolotarevAndjil <andjilzolotarev@yandex.ru>
2026-09-26 12:09:13 +0300

    .

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('blog.urls', namespace='blog')),
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
-]
+    path('', include('blog.urls', namespace='blog')),
+] 
